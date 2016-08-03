83168c1a15517bf9fd72abb99e684dc019a3e5d7
Oleg Mironov <oleg.mironov@eficode.com>
2016-08-03 16:18:13 +0300

    Tutorial Complete

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, OnDestroy, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Hero } from './hero';
@@ -9,9 +9,14 @@ import { HeroService } from './hero.service';
     templateUrl: 'app/hero-detail.component.html',
     styleUrls: ['app/hero-detail.component.css']
 })
+
 export class HeroDetailComponent implements OnInit, OnDestroy {
-    hero: Hero;
+    @Input() hero: Hero;
+    @Output() close = new EventEmitter();
+    error: any;
     sub: any;
+    navigated = false;
+
     constructor(
         private heroService: HeroService,
         private route: ActivatedRoute) {
@@ -19,9 +24,14 @@ export class HeroDetailComponent implements OnInit, OnDestroy {
 
     ngOnInit() {
         this.sub = this.route.params.subscribe(params => {
-            let id = +params['id'];
-            this.heroService.getHero(id)
-                .then(hero => this.hero = hero);
+            if (params['id'] !== undefined) {
+                let id = +params['id'];
+                this.navigated = true;
+                this.heroService.getHero(id).then(hero => this.hero = hero);
+            } else {
+                this.navigated = false;
+                this.hero = new Hero();
+            }
         });
     }
 
@@ -29,7 +39,17 @@ export class HeroDetailComponent implements OnInit, OnDestroy {
         this.sub.unsubscribe();
     }
 
-    goBack() {
-        window.history.back();
+    save() {
+        this.heroService.save(this.hero)
+        .then(hero => {
+            this.hero = hero;
+            this.goBack(hero);
+        })
+        .catch(error => this.error = error);
+    }
+
+    goBack(savedHero: Hero = null) {
+        this.close.emit(savedHero);
+        if (this.navigated) { window.history.back(); }
     }
 }
